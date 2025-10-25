078d99a9709e53d16725b0961cf499d5460789b9
VYX1 <85152961+VYX1@users.noreply.github.com>
2025-10-25 19:50:50 -0400

    clarity

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
 import math
-crit_chance = 0.16 # change with whatever the base cc of gun in question is
-#for weapons with total cc before enervate >200% added bonus is always be +25% total cc avg
+
+modded_crit = 0.16 * (1 + 2.4) # crit before enervate is counted
+# for weapons with total cc before enervate >200% added bonus is always be +25% total cc avg
 enervate = 0.10
 j = 0 # enervate cc counter
 k = 0 # enervate 'big crit' counter
-z = 0 # count amount of base / orange / red crits
+z = 0 # count amount of base / yellow / orange / red crits (0 / 1 / 2 / 3)
 p = 0 # for loop for multishot
 ms = 3.3 # modded. always 1 if beam weapon, regardless of modded multi. 
 ms_max = math.ceil(ms)
@@ -13,7 +14,7 @@ ms_min = math.floor(ms)
 
 for i in range(1000000):
     k_incremented = False
-    modded_crit = crit_chance * (1 + 2.4) + enervate*j # 2.4 is sentient surge cc (240%)
+    total_crit = modded_crit + enervate * j 
     j += 1
 
     if random.randint(0,100)/100 + ms_min > ms:
@@ -22,15 +23,15 @@ for i in range(1000000):
         simulated_ms = ms_max
     
     for p in range(simulated_ms):
-        if modded_crit <= 1:
+        if total_crit <= 1:
             simulated_crit = random.randint(0, 100)
-            if simulated_crit > modded_crit*100:
+            if simulated_crit > total_crit*100:
                 z += 0
             else:
                 z += 1
-        if modded_crit > 2:
+        if total_crit > 2:
             simulated_crit = random.randint(0, 100)
-            if simulated_crit > modded_crit*100-200:
+            if simulated_crit > total_crit*100-200:
                 z += 2
                 if not k_incremented:
                     k += 1
@@ -40,9 +41,9 @@ for i in range(1000000):
                 if not k_incremented:
                     k += 1
                     k_incremented = True
-        if modded_crit > 1:
+        if total_crit > 1:
             simulated_crit = random.randint(0, 100)
-            if simulated_crit > modded_crit*100-100:
+            if simulated_crit > total_crit*100-100:
                 z += 1
             else:
                 z += 2
@@ -54,4 +55,4 @@ for i in range(1000000):
         j = 0
         k = 0
     
-print(f"{z/1000000/ms}")
+print(f"{z/1000000/ms}") 
